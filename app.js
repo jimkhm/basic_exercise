a72af1eea12139de40489d9a7a00c72f9e1a28a4
Jim <jim@Jimui-MacBook-Air.local>
2018-01-07 16:32:09 +0900

    add register get router and register.pug

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
 app.get('/', function(req, res) {
   res.render('homepage');
-})
+});
 
 app.listen(3000, function() {
   console.log("Conntected 3000 port!");
-})
+});
