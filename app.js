097ee31eda80af70897a870fb93dded1c2da8d25
Jim <jim@Jimui-MacBook-Air.local>
2018-01-07 15:58:28 +0900

    set view engine to pug and set views folder to views

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', '/views');
+
+
+
 app.listen(3000, function() {
   console.log("Conntected 3000 port!");
 })
