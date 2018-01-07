89b5547b4ec43474488996037839f848c71f7e33
Jim <jim@Jimui-MacBook-Air.local>
2018-01-07 16:12:34 +0900

    add homepage.pug and correct views path /views to ./views

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'pug');
-app.set('views', '/views');
+app.set('views', './views');
 
-
+app.get('/', function(req, res) {
+  res.render('homepage');
+})
 
 app.listen(3000, function() {
   console.log("Conntected 3000 port!");
