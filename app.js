f073229457ab8729088605783ca8390cbf8f849b
Jim <jim@Jimui-MacBook-Air.local>
2018-01-07 20:17:07 +0900

    add register post router that is connected to mysql

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'1121ujung',
+  database:'checkList'
+});
+conn.connect();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -16,8 +24,31 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
+  console.log('/register');
   console.log(req.body);
-  res.send(req.body);
+
+  const email= req.body.email;
+  const password= req.body.password;
+  const date = new Date();
+
+  const sql= 'SELECT * FROM user WHERE email=?';
+  conn.query(sql, [email], function(err, results, fields) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else if(results.length > 0) {
+      res.status(200).send("이미 등록된 이메일입니다");
+    } else {
+      const sql= 'INSERT INTO user (email, password, created_at) VALUES (?, ?, ?)';
+      conn.query(sql, [email, password, date], function(err, results, feilds) {
+        if(err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.status(200).redirect('/login');
+      });
+    }
+  });
 });
 
 app.get('/register', function(req, res) {
